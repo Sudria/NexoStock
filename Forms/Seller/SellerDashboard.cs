1509f5aeeb0438ae9704f1e43e5685d6ec6693a5
Sudria <sudria.gonzalo@gmail.com>
2026-08-27 03:47:46 -0300

    HotFix buttons locations all controls users

---
+++ Forms/Seller/SellerDashboard.cs
@@ -0,0 +1,75 @@
+﻿using FontAwesome.Sharp;
+using ReaLTaiizor.Controls;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+ 
+namespace NexoStock.Forms.Seller
+
+
+{
+    public partial class SellerDashboard : UserControl
+    {
+        public SellerDashboard()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void changeStateButton(CyberButton e) {
+            if (e.Lighting)
+            {
+               innactiveButton(e);
+            }
+            else
+            {
+                activeButton(e);
+            }
+           
+        }
+
+  
+
+
+        private void activeButton(CyberButton e)
+        {
+            e.ColorPen_1 = Color.FromArgb(192, 0, 192);
+            e.ColorPen_2 = Color.Violet;
+            e.LinearGradient_Background = true;
+            e.Lighting = true;
+            e.ColorBackground_1 = Color.Fuchsia;
+            e.ColorBackground_2 = Color.MediumSlateBlue;
+        }
+
+        private void innactiveButton(CyberButton e)
+        {
+            e.ColorPen_1 = Color.Transparent;
+            e.ColorPen_2 = Color.Transparent;
+            e.Lighting = false;
+            e.LinearGradient_Background = false;
+        }
+
+
+
+
+
+        private void buttonClick(object sender, EventArgs e)
+        {
+            changeStateButton((CyberButton)sender);
+
+        }
+
+
+        private void HomeButton_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
--- Forms/Warehouse/WarehouseDashboard.Designer.cs
+++ Forms/Warehouse/WarehouseDashboard.Designer.cs
@@ -1,6 +1,6 @@
-﻿namespace NexoStock.Forms.Admin
+﻿namespace NexoStock.Forms.Warehouse
 {
-    partial class AdminDashboard
+    partial class WarehouseDashboard
     {
         /// <summary> 
         /// Variable del diseñador necesaria.
@@ -28,15 +28,12 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AdminDashboard));
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(WarehouseDashboard));
             this.HomeButton = new ReaLTaiizor.Controls.CyberButton();
-            this.userButton = new ReaLTaiizor.Controls.CyberButton();
             this.productButton = new ReaLTaiizor.Controls.CyberButton();
-            this.categoryButton = new ReaLTaiizor.Controls.CyberButton();
             this.providerButton = new ReaLTaiizor.Controls.CyberButton();
-            this.hopePictureBox1 = new ReaLTaiizor.Controls.HopePictureBox();
-            this.operationButton = new ReaLTaiizor.Controls.CyberButton();
             this.moveButton = new ReaLTaiizor.Controls.CyberButton();
+            this.hopePictureBox1 = new ReaLTaiizor.Controls.HopePictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.hopePictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -83,49 +80,6 @@
             this.HomeButton.Timer_RGB = 1;
             this.HomeButton.Click += new System.EventHandler(this.HomeButton_Click);
             // 
-            // userButton
-            // 
-            this.userButton.Alpha = 30;
-            this.userButton.BackColor = System.Drawing.Color.Transparent;
-            this.userButton.Background = true;
-            this.userButton.Background_WidthPen = 2F;
-            this.userButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.userButton.BackgroundPen = true;
-            this.userButton.ColorBackground = System.Drawing.Color.Empty;
-            this.userButton.ColorBackground_1 = System.Drawing.Color.Fuchsia;
-            this.userButton.ColorBackground_2 = System.Drawing.Color.MediumSlateBlue;
-            this.userButton.ColorBackground_Pen = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(200)))), ((int)(((byte)(238)))));
-            this.userButton.ColorLighting = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(50)))), ((int)(((byte)(190)))));
-            this.userButton.ColorPen_1 = System.Drawing.Color.Transparent;
-            this.userButton.ColorPen_2 = System.Drawing.Color.Transparent;
-            this.userButton.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.userButton.CyberButtonStyle = ReaLTaiizor.Enum.Cyber.StateStyle.Custom;
-            this.userButton.Effect_1 = true;
-            this.userButton.Effect_1_ColorBackground = System.Drawing.Color.Violet;
-            this.userButton.Effect_1_Transparency = 25;
-            this.userButton.Effect_2 = false;
-            this.userButton.Effect_2_ColorBackground = System.Drawing.Color.LimeGreen;
-            this.userButton.Effect_2_Transparency = 20;
-            this.userButton.Font = new System.Drawing.Font("Arial", 11F);
-            this.userButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(245)))), ((int)(((byte)(245)))));
-            this.userButton.Lighting = false;
-            this.userButton.LinearGradient_Background = false;
-            this.userButton.LinearGradientPen = true;
-            this.userButton.Location = new System.Drawing.Point(50, 145);
-            this.userButton.Name = "userButton";
-            this.userButton.PenWidth = 15;
-            this.userButton.Rounding = true;
-            this.userButton.RoundingInt = 25;
-            this.userButton.Size = new System.Drawing.Size(235, 50);
-            this.userButton.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
-            this.userButton.TabIndex = 3;
-            this.userButton.Tag = "Cyber";
-            this.userButton.TextButton = "USUARIOS";
-            this.userButton.TextRenderingHint = System.Drawing.Text.TextRenderingHint.AntiAlias;
-            this.userButton.Timer_Effect_1 = 1;
-            this.userButton.Timer_RGB = 1;
-            this.userButton.Click += new System.EventHandler(this.buttonClick);
-            // 
             // productButton
             // 
             this.productButton.Alpha = 30;
@@ -154,63 +108,20 @@
             this.productButton.Lighting = false;
             this.productButton.LinearGradient_Background = false;
             this.productButton.LinearGradientPen = true;
-            this.productButton.Location = new System.Drawing.Point(50, 202);
+            this.productButton.Location = new System.Drawing.Point(50, 156);
             this.productButton.Name = "productButton";
             this.productButton.PenWidth = 15;
             this.productButton.Rounding = true;
             this.productButton.RoundingInt = 25;
             this.productButton.Size = new System.Drawing.Size(235, 50);
             this.productButton.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
-            this.productButton.TabIndex = 4;
+            this.productButton.TabIndex = 3;
             this.productButton.Tag = "Cyber";
             this.productButton.TextButton = "PRODUCTOS";
             this.productButton.TextRenderingHint = System.Drawing.Text.TextRenderingHint.AntiAlias;
             this.productButton.Timer_Effect_1 = 1;
             this.productButton.Timer_RGB = 1;
             this.productButton.Click += new System.EventHandler(this.buttonClick);
-            // 
-            // categoryButton
-            // 
-            this.categoryButton.Alpha = 30;
-            this.categoryButton.BackColor = System.Drawing.Color.Transparent;
-            this.categoryButton.Background = true;
-            this.categoryButton.Background_WidthPen = 2F;
-            this.categoryButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.categoryButton.BackgroundPen = true;
-            this.categoryButton.ColorBackground = System.Drawing.Color.Empty;
-            this.categoryButton.ColorBackground_1 = System.Drawing.Color.Fuchsia;
-            this.categoryButton.ColorBackground_2 = System.Drawing.Color.MediumSlateBlue;
-            this.categoryButton.ColorBackground_Pen = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(200)))), ((int)(((byte)(238)))));
-            this.categoryButton.ColorLighting = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(50)))), ((int)(((byte)(190)))));
-            this.categoryButton.ColorPen_1 = System.Drawing.Color.Transparent;
-            this.categoryButton.ColorPen_2 = System.Drawing.Color.Transparent;
-            this.categoryButton.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.categoryButton.CyberButtonStyle = ReaLTaiizor.Enum.Cyber.StateStyle.Custom;
-            this.categoryButton.Effect_1 = true;
-            this.categoryButton.Effect_1_ColorBackground = System.Drawing.Color.Violet;
-            this.categoryButton.Effect_1_Transparency = 25;
-            this.categoryButton.Effect_2 = false;
-            this.categoryButton.Effect_2_ColorBackground = System.Drawing.Color.LimeGreen;
-            this.categoryButton.Effect_2_Transparency = 20;
-            this.categoryButton.Font = new System.Drawing.Font("Arial", 11F);
-            this.categoryButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(245)))), ((int)(((byte)(245)))));
-            this.categoryButton.Lighting = false;
-            this.categoryButton.LinearGradient_Background = false;
-            this.categoryButton.LinearGradientPen = true;
-            this.categoryButton.Location = new System.Drawing.Point(50, 259);
-            this.categoryButton.Name = "categoryButton";
-            this.categoryButton.PenWidth = 15;
-            this.categoryButton.Rounding = true;
-            this.categoryButton.RoundingInt = 25;
-            this.categoryButton.Size = new System.Drawing.Size(235, 50);
-            this.categoryButton.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
-            this.categoryButton.TabIndex = 5;
-            this.categoryButton.Tag = "Cyber";
-            this.categoryButton.TextButton = "CATEGORIAS";
-            this.categoryButton.TextRenderingHint = System.Drawing.Text.TextRenderingHint.AntiAlias;
-            this.categoryButton.Timer_Effect_1 = 1;
-            this.categoryButton.Timer_RGB = 1;
-            this.categoryButton.Click += new System.EventHandler(this.buttonClick);
             // 
             // providerButton
             // 
@@ -240,77 +151,20 @@
             this.providerButton.Lighting = false;
             this.providerButton.LinearGradient_Background = false;
             this.providerButton.LinearGradientPen = true;
-            this.providerButton.Location = new System.Drawing.Point(50, 316);
+            this.providerButton.Location = new System.Drawing.Point(50, 224);
             this.providerButton.Name = "providerButton";
             this.providerButton.PenWidth = 15;
             this.providerButton.Rounding = true;
             this.providerButton.RoundingInt = 25;
             this.providerButton.Size = new System.Drawing.Size(235, 50);
             this.providerButton.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
-            this.providerButton.TabIndex = 6;
+            this.providerButton.TabIndex = 4;
             this.providerButton.Tag = "Cyber";
             this.providerButton.TextButton = "PROVEEDORES";
             this.providerButton.TextRenderingHint = System.Drawing.Text.TextRenderingHint.AntiAlias;
             this.providerButton.Timer_Effect_1 = 1;
             this.providerButton.Timer_RGB = 1;
             this.providerButton.Click += new System.EventHandler(this.buttonClick);
-            // 
-            // hopePictureBox1
-            // 
-            this.hopePictureBox1.BackColor = System.Drawing.Color.Transparent;
-            this.hopePictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.hopePictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("hopePictureBox1.Image")));
-            this.hopePictureBox1.Location = new System.Drawing.Point(0, 544);
-            this.hopePictureBox1.Name = "hopePictureBox1";
-            this.hopePictureBox1.PixelOffsetType = System.Drawing.Drawing2D.PixelOffsetMode.HighQuality;
-            this.hopePictureBox1.Size = new System.Drawing.Size(326, 119);
-            this.hopePictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.hopePictureBox1.SmoothingType = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
-            this.hopePictureBox1.TabIndex = 9;
-            this.hopePictureBox1.TabStop = false;
-            this.hopePictureBox1.TextRenderingType = System.Drawing.Text.TextRenderingHint.ClearTypeGridFit;
-            // 
-            // operationButton
-            // 
-            this.operationButton.Alpha = 30;
-            this.operationButton.BackColor = System.Drawing.Color.Transparent;
-            this.operationButton.Background = true;
-            this.operationButton.Background_WidthPen = 2F;
-            this.operationButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.operationButton.BackgroundPen = true;
-            this.operationButton.ColorBackground = System.Drawing.Color.Empty;
-            this.operationButton.ColorBackground_1 = System.Drawing.Color.Fuchsia;
-            this.operationButton.ColorBackground_2 = System.Drawing.Color.MediumSlateBlue;
-            this.operationButton.ColorBackground_Pen = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(200)))), ((int)(((byte)(238)))));
-            this.operationButton.ColorLighting = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(50)))), ((int)(((byte)(190)))));
-            this.operationButton.ColorPen_1 = System.Drawing.Color.Transparent;
-            this.operationButton.ColorPen_2 = System.Drawing.Color.Transparent;
-            this.operationButton.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.operationButton.CyberButtonStyle = ReaLTaiizor.Enum.Cyber.StateStyle.Custom;
-            this.operationButton.Effect_1 = true;
-            this.operationButton.Effect_1_ColorBackground = System.Drawing.Color.Violet;
-            this.operationButton.Effect_1_Transparency = 25;
-            this.operationButton.Effect_2 = false;
-            this.operationButton.Effect_2_ColorBackground = System.Drawing.Color.LimeGreen;
-            this.operationButton.Effect_2_Transparency = 20;
-            this.operationButton.Font = new System.Drawing.Font("Arial", 11F);
-            this.operationButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(245)))), ((int)(((byte)(245)))));
-            this.operationButton.Lighting = false;
-            this.operationButton.LinearGradient_Background = false;
-            this.operationButton.LinearGradientPen = true;
-            this.operationButton.Location = new System.Drawing.Point(50, 373);
-            this.operationButton.Name = "operationButton";
-            this.operationButton.PenWidth = 15;
-            this.operationButton.Rounding = true;
-            this.operationButton.RoundingInt = 25;
-            this.operationButton.Size = new System.Drawing.Size(235, 50);
-            this.operationButton.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
-            this.operationButton.TabIndex = 10;
-            this.operationButton.Tag = "Cyber";
-            this.operationButton.TextButton = "OPERACIONES";
-            this.operationButton.TextRenderingHint = System.Drawing.Text.TextRenderingHint.AntiAlias;
-            this.operationButton.Timer_Effect_1 = 1;
-            this.operationButton.Timer_RGB = 1;
             // 
             // moveButton
             // 
@@ -340,37 +194,50 @@
             this.moveButton.Lighting = false;
             this.moveButton.LinearGradient_Background = false;
             this.moveButton.LinearGradientPen = true;
-            this.moveButton.Location = new System.Drawing.Point(50, 430);
+            this.moveButton.Location = new System.Drawing.Point(50, 292);
             this.moveButton.Name = "moveButton";
             this.moveButton.PenWidth = 15;
             this.moveButton.Rounding = true;
             this.moveButton.RoundingInt = 25;
             this.moveButton.Size = new System.Drawing.Size(235, 50);
             this.moveButton.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
-            this.moveButton.TabIndex = 11;
+            this.moveButton.TabIndex = 5;
             this.moveButton.Tag = "Cyber";
             this.moveButton.TextButton = "MOVIMIENTOS";
             this.moveButton.TextRenderingHint = System.Drawing.Text.TextRenderingHint.AntiAlias;
             this.moveButton.Timer_Effect_1 = 1;
             this.moveButton.Timer_RGB = 1;
+            this.moveButton.Click += new System.EventHandler(this.buttonClick);
             // 
-            // AdminDashboard
+            // hopePictureBox1
+            // 
+            this.hopePictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.hopePictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.hopePictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("hopePictureBox1.Image")));
+            this.hopePictureBox1.Location = new System.Drawing.Point(0, 543);
+            this.hopePictureBox1.Name = "hopePictureBox1";
+            this.hopePictureBox1.PixelOffsetType = System.Drawing.Drawing2D.PixelOffsetMode.HighQuality;
+            this.hopePictureBox1.Size = new System.Drawing.Size(326, 119);
+            this.hopePictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.hopePictureBox1.SmoothingType = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+            this.hopePictureBox1.TabIndex = 9;
+            this.hopePictureBox1.TabStop = false;
+            this.hopePictureBox1.TextRenderingType = System.Drawing.Text.TextRenderingHint.ClearTypeGridFit;
+            // 
+            // WarehouseDashboard
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(7)))), ((int)(((byte)(11)))), ((int)(((byte)(31)))));
             this.BackgroundImage = global::NexoStock.Properties.Resources.DashboardBackground;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.Controls.Add(this.moveButton);
-            this.Controls.Add(this.operationButton);
             this.Controls.Add(this.hopePictureBox1);
+            this.Controls.Add(this.moveButton);
             this.Controls.Add(this.providerButton);
-            this.Controls.Add(this.categoryButton);
             this.Controls.Add(this.productButton);
-            this.Controls.Add(this.userButton);
             this.Controls.Add(this.HomeButton);
             this.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.Name = "AdminDashboard";
+            this.Name = "WarehouseDashboard";
             this.Size = new System.Drawing.Size(326, 662);
             ((System.ComponentModel.ISupportInitialize)(this.hopePictureBox1)).EndInit();
             this.ResumeLayout(false);
@@ -379,12 +246,9 @@
 
         #endregion
         private ReaLTaiizor.Controls.CyberButton HomeButton;
-        private ReaLTaiizor.Controls.CyberButton userButton;
         private ReaLTaiizor.Controls.CyberButton productButton;
-        private ReaLTaiizor.Controls.CyberButton categoryButton;
         private ReaLTaiizor.Controls.CyberButton providerButton;
-        private ReaLTaiizor.Controls.HopePictureBox hopePictureBox1;
-        private ReaLTaiizor.Controls.CyberButton operationButton;
         private ReaLTaiizor.Controls.CyberButton moveButton;
+        private ReaLTaiizor.Controls.HopePictureBox hopePictureBox1;
     }
 }
